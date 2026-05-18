3d7def73607cc8c0c38ebcd69ce134033e701f48
hms1499 <thanvanhuy159@gmail.com>
2026-05-18 10:43:49 +0700

    Revert "test: add leaderboard top-ten scoring helper"
    This reverts commit d2855a8d3d1436c29751dedd6407baa0b9eef401.

--- frontend/lib/leaderboard.ts
+++
@@ -1,7 +0,0 @@
-import type { TopEntry } from "./contract-calls";
-
-export function isTopTenScore(score: number, top: TopEntry[]): boolean {
-  if (top.length < 10) return true;
-  const lowest = Math.min(...top.map((entry) => entry.score));
-  return score > lowest;
-}
