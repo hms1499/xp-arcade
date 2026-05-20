5885b051ffde635e348735109bd76f45e43c1c41
hms1499 <thanvanhuy159@gmail.com>
2026-05-20 14:29:44 +0700

    feat(tetris): wire TetrisWindow and leaderboard/NFT windows into page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 import { BootScreen } from "@/components/desktop/BootScreen";
 import { Desktop } from "@/components/desktop/Desktop";
 import { SnakeWindow } from "@/components/game/snake/SnakeWindow";
+import { TetrisWindow } from "@/components/game/tetris/TetrisWindow";
 import { SharedLeaderboard } from "@/components/shared/SharedLeaderboard";
 import { SharedMyNfts } from "@/components/shared/SharedMyNfts";
 import { SeasonAdminWindow } from "@/components/windows/SeasonAdminWindow";
@@ -12,8 +13,11 @@ export default function Home() {
     <BootScreen>
       <Desktop>
         <SnakeWindow />
+        <TetrisWindow />
         <SharedLeaderboard gameId="snake" />
+        <SharedLeaderboard gameId="tetris" />
         <SharedMyNfts gameId="snake" />
+        <SharedMyNfts gameId="tetris" />
         <SeasonAdminWindow />
         <PlayerProfileWindow />
         <Balloons />
