b74af929ceab8b60d97fca1aeb13bcd6a938e43f
hms1499 <thanvanhuy159@gmail.com>
2026-05-20 15:01:20 +0700

    feat(pacman): wire PacManWindow and leaderboard/NFT windows into page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ import { BootScreen } from "@/components/desktop/BootScreen";
 import { Desktop } from "@/components/desktop/Desktop";
 import { SnakeWindow } from "@/components/game/snake/SnakeWindow";
 import { TetrisWindow } from "@/components/game/tetris/TetrisWindow";
+import { PacManWindow } from "@/components/game/pacman/PacManWindow";
 import { SharedLeaderboard } from "@/components/shared/SharedLeaderboard";
 import { SharedMyNfts } from "@/components/shared/SharedMyNfts";
 import { SeasonAdminWindow } from "@/components/windows/SeasonAdminWindow";
@@ -14,10 +15,13 @@ export default function Home() {
       <Desktop>
         <SnakeWindow />
         <TetrisWindow />
+        <PacManWindow />
         <SharedLeaderboard gameId="snake" />
         <SharedLeaderboard gameId="tetris" />
+        <SharedLeaderboard gameId="pacman" />
         <SharedMyNfts gameId="snake" />
         <SharedMyNfts gameId="tetris" />
+        <SharedMyNfts gameId="pacman" />
         <SeasonAdminWindow />
         <PlayerProfileWindow />
         <Balloons />
