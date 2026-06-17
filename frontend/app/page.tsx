4bfc17491f5d8535eb214d5e4b2f40ad1d970e63
hms1499 <thanvanhuy159@gmail.com>
2026-06-17 09:53:29 +0700

    feat(solitaire): mount SolitaireWindow on the desktop

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,6 +5,7 @@ import { TetrisWindow } from "@/components/game/tetris/TetrisWindow";
 import { PacManWindow } from "@/components/game/pacman/PacManWindow";
 import { BreakoutWindow } from "@/components/game/breakout/BreakoutWindow";
 import { MinesweeperWindow } from "@/components/game/minesweeper/MinesweeperWindow";
+import { SolitaireWindow } from "@/components/game/solitaire/SolitaireWindow";
 import { HighScoreWindow } from "@/components/windows/HighScoreWindow";
 import { HallOfFameWindow } from "@/components/windows/HallOfFameWindow";
 import { MyNftsWindow } from "@/components/windows/MyNftsWindow";
@@ -21,6 +22,7 @@ export default function Home() {
         <PacManWindow />
         <BreakoutWindow />
         <MinesweeperWindow />
+        <SolitaireWindow />
         <HighScoreWindow />
         <HallOfFameWindow />
         <MyNftsWindow />
