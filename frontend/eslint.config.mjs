42fd90ed36c4060fd839b903376313d49e3c6684
hms1499 <thanvanhuy159@gmail.com>
2026-06-18 22:43:46 +0700

    chore(lint): ignore stale ".next 2" build artifact directory

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,6 +9,7 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".next 2/**",
     "test-results/**",
     "playwright-report/**",
     "out/**",
